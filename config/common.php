6a364a84dc3f67e300a5e188785a2b80999c275b
PhiNguyen2001 <phinguyen.workk@gmail.com>
2021-10-03 01:54:26 +0700

    UpdateSalons+TimeBookings

---
+++ config/common.php
@@ -0,0 +1,11 @@
+<?php
+    return [
+        'salon' => [
+            'status' => [
+                'dang_hoat_dong' => 1,
+                'dung_hoat_dong' => 0,
+
+            ]
+        ],
+    ]
+?>
